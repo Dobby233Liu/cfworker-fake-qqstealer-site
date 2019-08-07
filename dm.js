38582e35ce440a601935603831ae04e60670578f
Liu-Wen-Yuan <15816141883@163.com>
2019-08-07 09:28:27 +0800

    Update and rename dm.js to gh-hack-UNUSED.js

--- dm.js
+++
@@ -1,9 +0,0 @@
-class Unused{
-onclick(z){
-z.display="0";
-}
-onblur(z){
-z.blur=false
-}
-}
-module.exports = Unused
---
+++ gh-hack-UNUSED.js
@@ -0,0 +1,12 @@
+class Unused{
+  onclick(z){
+    z.display="0";
+  }
+  onblur(z){
+    z.blur=false;
+  }
+  githubIsMinifed(code){
+    return false;
+  }
+}
+module.exports = Unused
